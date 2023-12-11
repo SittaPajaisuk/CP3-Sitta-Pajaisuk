04c17f53e7032c0a3d0ee227521fb54fcab88623
SittaPajaisuk <139678957+SittaPajaisuk@users.noreply.github.com>
2023-12-11 15:57:48 +0700

    Add files via upload

---
+++ Exercise4_Sitta_P.py
@@ -0,0 +1,9 @@
+Found_Eng = 89.9
+Gener_Bs = 80
+Inter_Com_sys = 79.6
+Com_Pro = 82.3
+print("-------------- Your Score --------------")
+print("Foundation English               :",Found_Eng)
+print("General Business                 :",Gener_Bs)
+print("Introduction to Computer Systems :",Inter_Com_sys)
+print("Computer Programming             :",Com_Pro)
