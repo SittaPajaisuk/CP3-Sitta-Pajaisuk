a99e89185c104c86616d06484d35ab5b0ca1f7b8
SittaPajaisuk <139678957+SittaPajaisuk@users.noreply.github.com>
2024-05-23 23:52:41 +0700

    Update Lecture71_Sitta_P.py

--- Lecture71_Sitta_P.py
+++ Lecture71_Sitta_P.py
@@ -1,27 +1,21 @@
 menuList = []
 priceList = []
-total = 0
+
+def showBill():
+    total = 0
+    print("----- My Food-----")
+    for number in range(len(menuList)):
+        print(menuList[number], priceList[number],"THB")
+        total += int(priceList[number])
+    print("Total :", total,"THB")
 
 while True:
-    menuName = input("Please Enter Menu : ")
-    if(menuName.lower() == "exit"):
+    menuName = input("Plese Enter Menu : ")
+    if (menuName.lower() == "exit"):
         break
-
     else:
         menuPrice = input("Price : ")
         menuList.append(menuName)
         priceList.append(menuPrice)
-
-    for i in range(len(priceList)):
-        total = total + (int(priceList[i]))
-
-
-
-
-def showBill():
-    print("---- My Food ----")
-    for number in range(len(menuList)):
-        print(menuList[number],priceList[number])
-    print("total :",total)
 
 showBill()
