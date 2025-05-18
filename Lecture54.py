96e8bc6cca72676ee3d2bcaa7c3e5d3446f5b360
SittaPajaisuk <139678957+SittaPajaisuk@users.noreply.github.com>
2025-05-19 03:35:40 +0700

    Update Lecture54.py

--- Lecture54.py
+++ Lecture54.py
@@ -1,34 +1,34 @@
 def login():
-    usernameInput = input("Username : ")
-    passwordInput = input("Password : ")
-    if usernameInput == "admin" and passwordInput == "1234":
-        print("Done !")
-        return showMenu()
-    else:
-        print("Wrong!!!")
-        reture login()
-
+  usernameInput = input("Username : ")
+  passwordInput = input("Password : ")
+  if usernameInput == "admin" and passwordInput == "1234":
+    print("Done!!!")
+    showMenu()
+  else:
+    print("Wrong!!!")
+    login()
+  
 def showMenu():
-    print("----- iShop -----")
-    print("1. Vat Calculator")
-    print("2. Price Calculator")
-    menuSelect()
+  print("----- iShop -----")
+  print("1. Vat Calculator")
+  print("2. Price Calculator")
+  menuSelect()
 
 def menuSelect():
-    userSelected = int(input(">>"))
-    if userSelected == 1:
-        print("Total Price :",vatCalculate(int(input("Price (THB) : "))))
-    elif userSelected == 2:
-        print("Total Price :",pricecalcuate())
+  userSelected = int(input(">>"))
+  if userSelected == 1:
+    print("Total Price :",vatCalculate(int(input("Price (THB) : "))))
+  elif userSelected == 2:
+    print("Total Price :",pricecalcuate())
 
-def vatCalculator(totalPrice):
-    vat = 7
-    result = totalPrice + (totalPrice * vat / 100)
-    return result
-
-def priceCalculator():
-    price1 = int(input("First Product Price : "))
-    price2 = int(input("Second Product Price : "))
-    return vatCalculator(price1 + price2)
+def vatCalculate(totalPrice):
+  vat = 7
+  result = totalPrice + (totalPrice * vat / 100)
+  return result
+  
+def pricecalcuate():
+  price1 = int(input("First Product Price : "))
+  price2 = int(input("Second Product Price : "))
+  return vatCalculate(price1 + price2)
 
 login()
